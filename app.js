7047f732569498b8c129c4cf0a7cc5f390f3f4cd
AbihaFatima <gl2987@myamu.ac.in>
2021-06-30 18:41:14 +0400

    commit

--- app.js
+++ app.js
@@ -210,7 +210,6 @@ if (port == null || port == "") {
   port = 3000;
 }
 
-
-app.listen(port,function(){
-    console.log("Server has started successfully");
+app.listen(port, function(){
+    console.log("Server has started successfully.");
 });
